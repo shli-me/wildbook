af32ab966951f8bdde53dfbd01b6fe64b0969bb9
SamirMohamedCS <SamirMohamedCS@gmail.com>
2014-05-13 02:08:20 -0400

    Count likes procedures

--- db_migration/stored_procedures.sql
+++ db_migration/stored_procedures.sql
@@ -6,9 +6,10 @@ DELIMITER $$
 
 CREATE PROCEDURE `populate_user` (IN user VARCHAR(100))
 proc_main:BEGIN
-SELECT email, firstname, lastname, gender, street, state, city, zipcode, birthdate
-FROM users
-WHERE username = user;
+
+  SELECT email, firstname, lastname, gender, street, state, city, zipcode, birthdate
+  FROM users
+  WHERE username = user;
 
 END proc_main $$
 
@@ -18,21 +19,53 @@ DELIMITER $$
 CREATE PROCEDURE `populate_user_friends` (IN user VARCHAR(100))
 proc_main:BEGIN
 
-SELECT sentby FROM friends WHERE receivedby = user AND status = 'Accepted'
-UNION
-SELECT receivedby FROM friends WHERE sentby = user AND status = 'Accepted';
+  SELECT sentby FROM friends WHERE receivedby = user AND status = 'Accepted'
+  UNION
+  SELECT receivedby FROM friends WHERE sentby = user AND status = 'Accepted';
 
 END proc_main $$
 
+DELIMITER $$
 
+Create Procedure `count_comment_likes` (In commentid int(10))
+proc_main:Begin
+
+  Select count(*)
+  From likes_comments
+  Where cid = commentid;
+
+End proc_main $$
+
+DELIMITER $$
+
+Create Procedure `count_loc_likes` (In activityid int(10), locationid int(10))
+proc_main:Begin
+
+  Select count(*)
+  From likes_locations_activities
+  Where actid = activityid And locid = locationid;
+
+End proc_main $$
+
+DELIMITER $$
+
+Create Procedure `count_post_likes` (In pid int(10))
+proc_main:Begin
+
+  Select count(*)
+  From likes_posts
+  Where postid = pid;
+
+End proc_main $$
 
 DELIMITER $$
 
 CREATE PROCEDURE `populate_post` (IN id int(10))
 proc_main:BEGIN
-SELECT author, receiver, text, content, posttime, permission_type, locid, actid
-FROM posts
-WHERE postid = id;
+
+  SELECT author, receiver, text, content, posttime, permission_type, locid, actid
+  FROM posts
+  WHERE postid = id;
 
 END proc_main $$
 
